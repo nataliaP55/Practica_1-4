cf1fc77df3c7f6c4e5af5c3361f55178afe91c8c
Natalia Pastrovicchio <nataliapastrovicchio@gmail.com>
2022-04-11 12:01:55 -0300

    terminé la práctica 1-4

---
+++ algoritmo.js
@@ -0,0 +1,19 @@
+document.write( `
+<details>
+        <summary>info 1</summary>
+
+        <p>
+            Texto de detalles
+        </p>
+        <img title="Barco" width="300" height="200" src="https://imagenes.20minutos.es/files/image_656_370/uploads/imagenes/2008/05/15/812472a.jpg" alt="imagen de barco">
+        <br>
+        <a href="http://google.com" title="Ingresá a la página de google">Google</a>
+
+    </details>
+    <details>
+        <summary>info 2</summary>
+        <p>
+            Texto de detalles
+        </p>
+    </details>
+` );
